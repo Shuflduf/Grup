4f833646a35294ed267c3e348565442fc968d55c
Shuflduf <shuflduf@gmail.com>
2024-08-27 21:51:36 -0600

    more things

--- node.gd
+++ node.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	var target = ""
 	var path = ""
 	var dir_mode = true
-
+	print(arguments.size())
 	if arguments.size() > 0:
 		target = arguments[0]
 	else:
@@ -18,7 +18,26 @@ func _ready() -> void:
 		path = arguments[1]
 
 	if !dir_mode:
-		var file = FileAccess.open(path, FileAccess.READ)
-		var words = file.get_as_text()
-		if words.contains(target):
-			print("Found ", target, " at ", words.find(target))
+		if scan_file(path, target):
+			print("Found ", target, " in ", path)
+		else:
+			print("Not found")
+		get_tree().quit(0)
+	else:
+		for i in DirAccess.get_files_at(OS.get_executable_path().get_base_dir()):
+			print("Searching in: ", i)
+			if scan_file(i, target):
+				print("Found ", target, " in ", i, " at character ", \
+						FileAccess.get_file_as_string(i).find(target))
+				return
+				#get_tree().quit(0)
+
+	get_tree().quit(2)
+
+func scan_file(path: String, what: String) -> bool:
+	var file = FileAccess.open(path, FileAccess.READ)
+	var words = file.get_as_text()
+	if words.contains(what):
+		return true
+	else:
+		return false
