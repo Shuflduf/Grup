f11ad74caf2972c46c0c705f8993cfbe31798199
Shuflduf <shuflduf@gmail.com>
2024-08-27 21:24:21 -0600

    argument parsing

--- node.gd
+++ node.gd
@@ -1,1 +1,14 @@
 extends Node
+
+func _ready() -> void:
+	var arguments = {}
+	for argument in OS.get_cmdline_args():
+		if argument.contains("="):
+			var key_value = argument.split("=")
+			arguments[key_value[0].trim_prefix("--")] = key_value[1]
+		else:
+			# Options without an argument will be present in the dictionary,
+			# with the value set to an empty string.
+			arguments[argument.trim_prefix("--")] = ""
+	print(arguments)
+	get_tree().quit(0)
